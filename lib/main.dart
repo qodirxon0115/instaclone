e9d1f0f763ffe2e69eaf0a2497eefa6966a9a426
qodirxon <qodirxonesonov1@gmail.com>
2023-06-18 08:45:44 +0500

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone/pages/home_page.dart';
 import 'package:instaclone/pages/signin_page.dart';
 import 'package:instaclone/pages/signup_page.dart';
 import 'package:instaclone/pages/splash_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
